9b364c396f5e6d9880f48b14686d72d4fe9c2ceb
giorgimjavia <giorgi.mjavia17@gmail.com>
2025-07-15 11:57:52 +0400

    Add AnnotationsStreams

--- src/main/java/ge/tbc/testautomation/runners/Main.java
+++ src/main/java/ge/tbc/testautomation/runners/Main.java
@@ -14,26 +14,34 @@ public class Main {
     public static void main(String[] args) {
         Field[] fields = Analyzable.class.getDeclaredFields();
 
-        List<String> match = Arrays.stream(fields)
-                .filter(field -> field.isAnnotationPresent(VariableNameAnnotation.class))
+        List<Field> annotatedFields = Arrays.stream(fields)
+                .filter(field -> field.isAnnotationPresent(VariableNameAnnotation.class)).collect(Collectors.toList());
+
+        List<Field> match = annotatedFields.stream()
                 .filter(field -> {
                     VariableNameAnnotation annotation = field.getAnnotation(VariableNameAnnotation.class);
                     return field.getName().equalsIgnoreCase(annotation.name());
                 })
-                .map(Field::getName)
                 .collect(Collectors.toList());
 
-        List<String> no_match = Arrays.stream(fields)
-                .filter(field ->  field.isAnnotationPresent(VariableNameAnnotation.class))
+        List<Field> no_match = annotatedFields.stream()
                 .filter(field -> {
                     VariableNameAnnotation annotation = field.getAnnotation(VariableNameAnnotation.class);
                     return !field.getName().equalsIgnoreCase(annotation.name());
                 })
-                .map(Field::getName)
                 .collect(Collectors.toList());
 
-        System.out.println("Annotations are matching " + match);
-        System.out.println("Annotations aren't matching" + no_match);
+        System.out.println("Annotations are matching: ");
+        for (Field field : match) {
+            String annotationName = field.getAnnotation(VariableNameAnnotation.class).name();
+            System.out.println(field.getName() + " annotation: " + annotationName);
+        }
+
+        System.out.println("\nAnnotations aren't matching: ");
+        for (Field field : no_match) {
+            String annotationName = field.getAnnotation(VariableNameAnnotation.class).name();
+            System.out.println(field.getName() + " annotation: " + annotationName);
+        }
 
         @SuppressWarnings("unused")
         double points = 46.28;
